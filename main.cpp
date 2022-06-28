d2b1ab4292493f2b90aeae99a2e6e4cbb3ba3487
SicoRelt <67675482+SicoRelt@users.noreply.github.com>
2022-06-28 16:16:44 +0200

    step_1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+#define C 1
+#define O  3
+#define  S 6
+#define I 9
+
+
+
+
 int main(){
+    long int h ;
+    h = 20L;
     cout<<" Hi World!! "<<endl;
+    cout<<O<<endl;
 }
